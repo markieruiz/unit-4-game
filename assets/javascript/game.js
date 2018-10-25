2867cb0b29911f760fba01eddb3cd80df42a12d4
Markie Ruiz <markieruiz@Markies-MacBook-Pro.local>
2018-10-24 20:56:52 -0500

    jquery objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,3 @@
-
-
 var losses = 0;
 var wins = 0;
 var images = ['assets/images/img1.png', 
@@ -7,13 +5,9 @@ var images = ['assets/images/img1.png',
     'assets/images/img3.png', 
     'assets/images/img4.png']
 
-
-
 reset ();
 
 function reset () {
-    $("crystals").empty();
-
     var rand_num_crystal;
     var running_ttl = 0;
 
@@ -24,72 +18,41 @@ function reset () {
     $(".running_ttl").html([running_ttl]); 
 
 
-for (var i = 0; i < 4; i++){
-    var rand_num_crystal = Math.floor(Math.random() * 11) + 1;
-    var crystal = $("<div>");
+    for (var i = 0; i < 4; i++){
+        var rand_num_crystal = Math.floor(Math.random() * 11) + 1;
+        var crystal = $("<div>");
+        console.log(rand_num_crystal)
 
-    console.log(rand_num_crystal)
+        crystal.attr({
+            "class": 'crystal',
+            "data-random": rand_num_crystal
+        });
 
-    crystal.attr({
-        "class": 'crystal',
-        "data-random": rand_num_crystal
-    });
+        crystal.css({
+            "background-image":"url('" + (images[i]) + "')",
+            "background-size":"cover"
+        });
 
-    crystal.css({
-         "background-image":"url('" + (images[i]) + "')",
-         "background-size":"cover"
-    });
-
-    $(".crystals").append(crystal);
-}
-
-
-$(".crystal").on('click', function () {
-    var num = parseInt($(this).attr('data-random'));
-    running_ttl += num;
-    $(".running_ttl").html([running_ttl]); 
-
-    if(running_ttl > rand_num_box) {
-        losses++;
-        $("crystals").empty();
-        $(".losses").html("Losses: " + losses);
-        reset();
+        $(".crystals").append(crystal);
     }
-    else if (running_ttl === rand_num_box){
+
+    $(".crystal").on('click', function () {
+        var num = parseInt($(this).attr('data-random'));
+        running_ttl += num;
+        $(".running_ttl").html([running_ttl]); 
+
+        if(running_ttl > rand_num_box) {
+            losses++;
+            $(".crystals").empty();
+            $(".losses").html("Losses: " + losses);
+            reset();
+        }
+        
+        else if (running_ttl === rand_num_box){
         wins++;
-        $("crystals").empty();
+        $(".crystals").empty();
         $(".wins").html("Wins: " + wins);
         reset();
-    }
-
-});
+        }   
+    });
 }
-
-
-
-
-// var wins = 0; 
-// console.log("wins: " + wins); 
-
-// var losses = 0; 
-// console.log("losses: " + losses); 
-
-// var score = 0; 
-// console.log("score: " + score); 
-// $(".running_ttl").html(score); 
-
-// $(".crystal").on('click', function () {
-//     var newScore = score += parseInt($(this).attr("rand_num_crystal")); 
-//     console.log("New Score: " + newScore); 
-//     $(".running_ttl").html(newScore);
-
-//     console.log($(this).attr('rand_num_crystal'));
-
-// });
-
-
-        
-        // $("img").on("click", function () {
-        //     var newScore = score += parseInt($(this).attr("value")); 
-        //         console.log("New Score: " + newScore); 
-        //     $(".running_ttl").html(newScore)});
